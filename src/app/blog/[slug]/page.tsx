c85490939bddcee46149e54aab844bb78f8bc5ec
RowinVA <rowin.amsterdam@gmail.com>
2023-10-06 21:28:22 +0200

    revert change, turn off content

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -39,43 +39,7 @@ const Slug = (props: BlogPostProps) => {
 
     return (
         <>
-            <Image
-                src={post.metadata.banner}
-                alt="Article thumbnail"
-                width={0}
-                height={0}
-                sizes="100vw"
-                className="h-56 w-full object-cover lg:h-96"
-                priority
-            />
-
-            <Container maxWidth="5xl" className=" mb-12 grid grid-cols-1 gap-8 md:mt-12 md:grid-cols-[1fr_minmax(0,_700px)_1fr]">
-                <div className="ml-auto md:row-start-2 md:ml-0">
-                    <ShareOnSocials
-                        url={`https://rowinvanamsterdam.com/blog/${post.metadata.slug}`}
-                        text={post.metadata.title}
-                        platforms={['twitter', 'facebook', 'linkedin', 'whatsapp']}
-                    />
-                </div>
-
-                <div className="md:col-start-1 md:col-end-4">
-                    <Typography component="h1" variant="h3" className="col-span-3 text-center">
-                        {post.metadata.title}
-                    </Typography>
-
-                    <ArticleMetadata
-                        author={post.metadata.author}
-                        authorImage={post.metadata.authorImage}
-                        date={getFormattedDate(post.metadata.date)}
-                        readingTimeInMinutes={getEstimateReadingTime(post.content)}
-                    />
-                </div>
-
-                <div className="self-center pb-3 md:col-start-2 md:col-end-2">
-                    <Markdown value={post.content} wrapper="article" openExternalLinksInNewTab />
-                    <Comments />
-                </div>
-            </Container>
+          <h1>hellow {params.slug}</h1>
         </>
     );
 };
