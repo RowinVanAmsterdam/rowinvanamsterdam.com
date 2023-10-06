a4e11c9ae459f58a8afb1ac33ddd912c78443b54
RowinVA <rowin.amsterdam@gmail.com>
2023-10-06 21:34:01 +0200

    -

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,11 +1,30 @@
+import { ArticleMetadata } from '@/Article/ArticleMetadata';
+import { Comments } from '@/Article/Comments';
+import { getPostBySlug } from '@/api/api';
+import { Container } from '@/shared/Container';
+import { Markdown } from '@/shared/RichContent/Markdown';
+import { ShareOnSocials } from '@/shared/Socials/ShareOnSocials';
+import { Typography } from '@/shared/Typography';
+import { getEstimateReadingTime } from '@/shared/helpers/getEstimateReadingTime';
+import { getFormattedDate } from '@/shared/helpers/getFormattedDate';
+import { getPageTitle } from '@/utils/getPageTitle';
+import { Metadata } from 'next';
+import Image from 'next/image';
+import profile from '../../../assets/static/rowinvanamsterdam.json';
+import { notFound } from 'next/navigation';
 
+type BlogPostProps = {
+    params: {
+        slug: string;
+    };
+};
 
-
-const Slug = () => {
+const Slug = (props: BlogPostProps) => {
+    const { params } = props;
 
     return (
         <>
-          <h1>helloworld</h1>
+          <h1>hellow {params.slug}</h1>
         </>
     );
 };
