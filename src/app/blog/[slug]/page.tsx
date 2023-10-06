3ef1157b366c7b23bde506662fdcd8ca18024b37
RowinVA <rowin.amsterdam@gmail.com>
2023-10-06 21:49:29 +0200

    -

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -21,7 +21,7 @@ type BlogPostProps = {
 
 export async function generateMetadata(props: BlogPostProps): Promise<Metadata> {
     const { params } = props;
-    const post = getPostBySlug(params.slug);
+    // const post = getPostBySlug(params.slug);
 
     return {
         title: getPageTitle(''),
