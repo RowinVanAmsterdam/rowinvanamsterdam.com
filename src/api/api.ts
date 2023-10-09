c4c0904152c3846c195fcebd0bf081b55ee79001
RowinVA <rowin.amsterdam@gmail.com>
2023-10-09 17:35:09 +0200

    Enabled drafts

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,10 +3,10 @@ import { parseMarkdown } from '../Article/helpers/parseMarkdown';
 import { environment } from '@/utils/environment';
 import path from 'path';
 
-const publishedPostsList = fs.readdirSync('posts/published');
-// const draftPostsList = fs.readdirSync('posts/drafts');
-// const showDraftPosts = environment.isDevelopment && process.env.SHOW_DRAFTS === 'true';
 const postsDirectory = path.join(process.cwd(), 'posts');
+const allPublishedPosts = fs.readdirSync(postsDirectory + '/published');
+const allDraftPosts = fs.readdirSync(postsDirectory + '/drafts');
+const showDraftPosts = environment.isDevelopment && process.env.SHOW_DRAFTS === 'true';
 
 const getPosts = (directory: string, fileList: string[]) => {
     return fileList.map((file) => {
@@ -16,9 +16,9 @@ const getPosts = (directory: string, fileList: string[]) => {
 };
 
 export const getAllBlogPosts = () => {
-    const publishedPosts = getPosts('published', publishedPostsList);
-    // const draftPosts = showDraftPosts ? getPosts('drafts', draftPostsList) : [];
-    const allPosts = [...publishedPosts];
+    const publishedPosts = getPosts('published', allPublishedPosts);
+    const draftPosts = showDraftPosts ? getPosts('drafts', allDraftPosts) : [];
+    const allPosts = [...publishedPosts, ...draftPosts];
     const postsSortedByDate = allPosts.sort((a, b) => Number(new Date(b.metadata.date)) - Number(new Date(a.metadata.date)));
 
     return postsSortedByDate;
